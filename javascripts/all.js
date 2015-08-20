5c1dabdf4f2b42c03e431f4dc8a00c54ec5886dd
Yaya Red <yaloveduff@gmail.com>
2015-08-20 17:36:27 +0800

    header-news 加入 sly 套件， desktop 待調整

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,3 +1,4 @@
+// off canvas menu //
 var off_canvas = function (){
   $(".menu-btn, .off-canvas-mask").on("click", function(event){
     $("body").toggleClass("open-menu");
@@ -6,3 +7,26 @@ var off_canvas = function (){
 };
 
 off_canvas();
+
+// sly.js //
+if ($(window).width() < 960){
+  var $frame = $('.frame'); window.frr = $frame;
+  var sly = new Sly($frame, {
+    horizontal: 1,
+    itemNav: 'centered',
+    activateMiddle: 1,
+    smart: 1,
+    activateOn: 'mouseenter',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 1,
+    // activatePageOn: 'click',
+    speed: 200,
+    moveBy: 600,
+    elasticBounds: 1,
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1,
+  }).init();
+}
